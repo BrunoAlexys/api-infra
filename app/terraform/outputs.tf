43dc4143e7f9a2b8491cca0c73b355a33704ce4b
BrunoAlexys <brunoalexysmoura@gmail.com>
2026-01-29 12:17:37 -0300

    feat: infra add

---
+++ app/terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "endpoint_da_api" {
+  value = "aws_lambda_function_url.api_url.function_url"
+}
---
+++ app/terraform/resources.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "api_repo" {
+  name = "repo-api-aula"
+}
+
+resource "aws_lambda_function" "api_data" {
+  function_name = "api-data-aula"
+  role          = aws_iam_role.iam_for_lambda.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.api_repo.repository_url}:latest"
+}
+
+resource "aws_lambda_function_url" "api_url" {
+  function_name      = aws_lambda_function.api_data.function_name
+  authorization_type = "NONE"
+}
